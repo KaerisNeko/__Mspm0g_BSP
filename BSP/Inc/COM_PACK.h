2d3b5df8dd7a96bd725c899e010ce474fbd5ffa6
KaerisNeko <3783312415@qq.com>
2026-07-19 10:13:20 -0400

    COM_PACK
    COM_PACK Initial commit

--- BSP/Inc/COM_PACK.h
+++ BSP/Inc/COM_PACK.h
@@ -3,4 +3,30 @@
 
 #include "ti_msp_dl_config.h"
 
+#define COMPACK_CMD_MAX_NUM             32
+#define COMPACK_CMD_IRRELEVANT_CHARS    " \r\n\"\t{}\\"
+
+
+
+typedef enum COMPACK_CmdType_t {
+    COMPACK_CMD_INT,
+    COMPACK_CMD_FLOAT,
+    COMPACK_CMD_DOUBLE,
+    COMPACK_CMD_STRING,
+    COMPACK_CMD_FUNC
+}COMPACK_CmdType;
+
+typedef struct COMPACK_CmdStruct_t {
+    char* name;
+    COMPACK_CmdType cmdType;
+    void* pData;
+}COMPACK_CmdStruct;
+
+typedef void (*COMPACK_CmdFuncCallback)(char*);
+
+
+
+void COMPACK_Init(void);
+void COMPACK_Decode(char* str);
+
 #endif
