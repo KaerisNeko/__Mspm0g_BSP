68e7074e9ae6d75088965a2a40e92e979ea0b51e
KaerisNeko <3783312415@qq.com>
2026-07-22 03:23:38 -0400

    Added PID

--- MID/Inc/PID.h
+++ MID/Inc/PID.h
@@ -2,5 +2,36 @@
 #define __PID_H__
 
 #include "ti_msp_dl_config.h"
+#include "DT.h"
+
+typedef struct PID_Struct_t {
+    float target;
+    float curVal;
+    float targetDelta;
+    float targetDeltaOld;
+    
+    DT_Context dtCtx;
+
+    float kp;
+    float ki;
+    float kd;
+    float intgLim;
+    
+    float intgAccum;
+    
+    float ctrlVal;
+
+    float ctrlValFloor;
+    float ctrlValCeil;
+}PID_Struct;
+
+void PID_Init(PID_Struct* pid,
+        float kp, float ki, float kd,
+        float intgLim);
+
+void PID_SetTarget(PID_Struct* pid, float target);
+void PID_SetCurVal(PID_Struct* pid, float curVal);
+float PID_Update(PID_Struct* pid);
+float PID_GetCtrlVal(PID_Struct* pid);
 
 #endif
--- MID/Src/PID.c
+++ MID/Src/PID.c
@@ -1,1 +1,76 @@
 #include "PID.h"
+#include "COM.h"
+#include <string.h>
+#include <stdio.h>
+
+void PID_Init(PID_Struct* pid,
+        float kp, float ki, float kd,
+        float intgLim) {
+    DT_Init(&pid->dtCtx);
+    pid->target = 0;
+    pid->curVal = 0;
+    pid->targetDelta = 0;
+    pid->targetDeltaOld = 0;
+
+    pid->kp = kp;
+    pid->ki = ki;
+    pid->kd = kd;
+    pid->intgLim = intgLim;
+
+    pid->intgAccum = 0;
+    pid->ctrlVal = 0;
+    pid->ctrlValFloor = 0;
+    pid->ctrlValCeil = 0;
+}
+
+void PID_Clamp(float* src, float floor, float ceil) {
+    if (*src > ceil) {
+        *src = ceil;
+    } else if (*src < floor) {
+        *src = floor;
+    }
+}
+
+void PID_SetTarget(PID_Struct* pid, float target) {
+    pid->target = target;
+    pid->targetDelta = pid->targetDeltaOld = target - pid->curVal;
+}
+
+void PID_SetCurVal(PID_Struct* pid, float curVal) {
+    pid->curVal = curVal;
+    pid->targetDelta = pid->target - curVal;
+}
+
+float PID_Update(PID_Struct* pid) {
+    uint32_t dt_us = DT_GetDeltaTime_us(&pid->dtCtx);
+    if (dt_us == 0) {
+        return 0;
+    }
+
+    float resDelta = 0;
+    
+    // Kp
+    resDelta += pid->targetDelta * pid->kp;
+    
+    // Ki
+    float dt = ((float)dt_us / 1e6f);
+    pid->intgAccum += pid->targetDelta * dt;
+    PID_Clamp(&pid->intgAccum, -pid->intgLim, pid->intgLim);
+    
+    resDelta += pid->intgAccum * pid->ki;
+
+    // Kd
+    float de = pid->targetDelta - pid->targetDeltaOld;
+    
+    resDelta += de / dt * pid->kd;
+
+    // Sum
+    pid->ctrlVal += resDelta;
+    PID_Clamp(&pid->ctrlVal, pid->ctrlValFloor, pid->ctrlValCeil);
+    pid->targetDeltaOld = pid->targetDelta;
+    return pid->ctrlVal;
+}
+
+float PID_GetCtrlVal(PID_Struct* pid) {
+    return pid->ctrlVal;
+}
