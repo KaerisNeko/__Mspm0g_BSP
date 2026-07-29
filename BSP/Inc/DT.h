5b59d3fa24e4564a9d95a20a3e5aafb6b27c6741
KaerisNeko <3783312415@qq.com>
2026-07-29 06:27:35 -0400

    DT added freq ctrl

--- BSP/Inc/DT.h
+++ BSP/Inc/DT.h
@@ -5,10 +5,13 @@
 
 typedef struct DT_Context_t {
     uint32_t timeOld;
+    uint16_t presc;
+    uint16_t cnt;
 }DT_Context;
 
 void DT_Init(DT_Context* ctx);
 uint32_t DT_GetDeltaTime_us(DT_Context* ctx);
-// void DT_UpdateRateSyncDelay(DT_Context* ctx, uint16_t rate);
+void DT_UpdateRateSyncDelay(DT_Context* ctx, uint16_t rate);
+uint8_t DT_FreqPrescale(DT_Context* ctx);
 
 #endif
--- BSP/Src/DT.c
+++ BSP/Src/DT.c
@@ -3,6 +3,8 @@
 
 void DT_Init(DT_Context* ctx) {
     ctx->timeOld = 0;
+    ctx->presc = 1;
+    ctx->cnt = 0;
 }
 
 uint32_t DT_GetDeltaTime_us(DT_Context* ctx) {
@@ -15,17 +17,20 @@ uint32_t DT_GetDeltaTime_us(DT_Context* ctx) {
     return dt;
 }
 
-// void DT_UpdateRateSyncDelay(DT_Context* ctx, uint16_t rate) {
-//     uint32_t delayTime_us = 1000000 / rate;
-//     while (1) {
-//         if (ctx->timeOld == 0) {
-//             ctx->timeOld = HRT_GetTime_us();
-//             continue;
-//         }
-//         uint32_t dt = HRT_GetTime_us() - ctx->timeOld;
-//         if (dt >= delayTime_us) {
-//             ctx->timeOld += dt;
-//             break;
-//         }
-//     }
-// }
+void DT_UpdateRateSyncDelay(DT_Context* ctx, uint16_t rate) {
+    uint32_t delayTime_us = 1000000 / rate;
+    if (ctx->timeOld == 0) {
+        ctx->timeOld = HRT_GetTime_us();
+    }
+    uint32_t endTime = ctx->timeOld + delayTime_us;
+    while (HRT_GetTime_us() < endTime);
+    ctx->timeOld = endTime;
+}
+
+uint8_t DT_FreqPrescale(DT_Context* ctx) {
+    if (++(ctx->cnt) >= ctx->presc) {
+        ctx->cnt = 0;
+        return 1;
+    }
+    return 0;
+}
